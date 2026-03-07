96dd51498183e2ef0b9dbec529d13b79c249c29a
massieuxalexandre <massieuxalexandre@gmail.com>
2026-03-07 18:59:30 +0100

    1er submit

--- knn.py
+++ knn.py
@@ -13,11 +13,16 @@ class Knn:
     def euclidean_distance(self, x_1, x_2):
         return np.sqrt(np.sum((np.array(x_1[:-1]) - np.array(x_2[:-1]))**2))
     
+    def manhattan_distance(self, x_1, x_2):
+        return np.sum(np.abs(np.array(x_1[:-1]) - np.array(x_2[:-1])))
+    
+    
     def knn_prediction(self, point_test):
         distances = []
 
         for i in range(len(self.data_train)):
             dist = self.euclidean_distance(point_test, self.data_train.iloc[i])
+            # dist = self.manhattan_distance(point_test, self.data_train.iloc[i])
             distances.append((dist, self.data_labels.iloc[i]))
 
         distances.sort(key=lambda x: x[0])
--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from data import *
 
 if __name__ == "__main__":
     df = pd.read_csv("sample_submission.csv")
+    # df = pd.read_csv("data_iris.txt")
     data = Data(df, "Label")
+    # data = Data(df, "Iris-setosa")
+
+    
 
     for j in range(3):
+        good = 0
+        total = 0
+
         data.set_train_test()
         knn = Knn(4, data.get_data_train(), data.get_data_test(), data.get_data_labels())
 
@@ -18,6 +25,11 @@ if __name__ == "__main__":
         # for i in range(len(data.get_data_test())):
         for i in range(5):
             print(data.get_data_test_labels().iloc[i], "-", knn.knn_prediction(data.get_data_test().iloc[i, :]))
+            total += 1
+            if (data.get_data_test_labels().iloc[i] == knn.knn_prediction(data.get_data_test().iloc[i, :])):
+                good += 1
+        accuracy = good / total
+        print("accuracy test n°", i, " : ", accuracy*100, " %", sep="")
         print()
 
 
