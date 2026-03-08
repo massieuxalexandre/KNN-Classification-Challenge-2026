1d28b74bbc5eef9abd9028b9be998e9b3c8f4b09
massieuxalexandre <massieuxalexandre@gmail.com>
2026-03-08 10:30:35 +0100

    guess 08-03

--- data.py
+++ data.py
@@ -2,32 +2,35 @@ import numpy as np
 import pandas as pd
 
 class Data:
-    def __init__(self, data, label_col):
+    def __init__(self, data, id_col, label_col):
         self.data = data 
         self.data_train = None 
         self.data_test = None 
         self.data_labels = None
         self.data_test_labels = None
+        self.data_id = None
         self.label_col = label_col
-        self.cpt = 0
+        self.id_col = id_col
 
 
-    def set_train_test(self):
+    def set_train_test(self, j):
         length = self.data.shape[0]
         tab = [(0, 0.8), (0.1, 0.9), (0.2, 1)]
         # tab = [(0, 0.74), (0.16, 0.9), (0.26, 1)]
-        start = int(tab[self.cpt][0] * length)
-        end = int(tab[self.cpt][1] * length)
+        start = int(tab[j][0] * length)
+        end = int(tab[j][1] * length)
 
-        self.data_train = self.data.iloc[start:end, :].drop(columns=[self.label_col])
+        self.data_train = self.data.iloc[start:end, :].drop(columns=[self.id_col, self.label_col])
         self.data_labels = self.data.iloc[start:end][self.label_col]
+        self.data_id = self.data.iloc[start:end][self.id_col]
 
         test_part1 = self.data.iloc[0:start, :]
         test_part2 = self.data.iloc[end:, :]
         self.data_test_labels = pd.concat([test_part1[self.label_col], test_part2[self.label_col]])
-        self.data_test = pd.concat([test_part1.drop(columns=[self.label_col]), test_part2.drop(columns=[self.label_col])])
+        self.data_test_id = pd.concat([test_part1[self.id_col], test_part2[self.id_col]])
+        self.data_test = pd.concat([test_part1.drop(columns=[self.id_col, self.label_col]), test_part2.drop(columns=[self.id_col, self.label_col])])
 
-        self.cpt += 1
+
 
 
     def get_data_train(self):
@@ -45,8 +48,9 @@ class Data:
     def get_label_col(self):
         return self.label_col
     
-    def get_data(self):
-        return self.data
+    def get_id_col(self):
+        return self.id_col
+    
 
     
 
--- main.py
+++ main.py
@@ -10,33 +10,38 @@ if __name__ == "__main__":
     df = pd.read_csv("train.csv")
     df_test = pd.read_csv("test.csv")
     # df = pd.read_csv("data_iris.txt")
-    data = Data(df, "Label")
-    data_test = Data(df_test, "Label")
+    data = Data(df, "Id", "Label")
+    # data_test = Data(df_test, "Label")
     # data = Data(df, "Iris-setosa")
     k = 3
-    
-
+    accuracy = []
+    print("3 test pour trouver les meilleurs settings :")
     for j in range(3):
         good = 0
         total = 0
-
-        # data.set_train_test()
+        guess = dict()
+        data.set_train_test(j)
         
-        # knn = Knn(k, data.get_data_train(), data.get_data_test(), data.get_data_labels())
-        knn = Knn(k, data.get_data(), data_test.get_data(), data.get_data_labels())
+        knn = Knn(k, data.get_data_train(), data.get_data_test(), data.get_data_labels())
 
         print("test n°", j+1, " : ", sep="")
         # print("label reel - label trouve")
-        # for i in range(len(data.get_data_test())):
-        for i in range(len(data_test.get_data())):
+        for i in range(len(data.get_data_test())):
         # for i in range(5):
-            # print(data.get_data_test_labels().iloc[i], "-", knn.knn_prediction(data.get_data_test().iloc[i, :]))
+            # print(data.get_data_test_labels().iloc[i], "-", knn.knn_prediction(data.get_data_test().iloc[i]))
             total += 1
-            if (data.get_data_test_labels().iloc[i] == knn.knn_prediction(data.get_data_test().iloc[i, :])):
+            if (data.get_data_test_labels().iloc[i] == knn.knn_prediction(data.get_data_test().iloc[i])):
                 good += 1
-        accuracy = good / total
-        print("accuracy test n°", j+1, " : ", accuracy*100, " %", sep="")
+        accuracy.append(good / total)
+        print("accuracy test n°", j+1, " : ", accuracy[j]*100, " %", sep="")
         print()
 
 
-    
+    print("test final :")
+    knn = Knn(k, data.get_data_train(), df_test.iloc[:, 1:], data.get_data_labels())
+    for i in range(len(df_test)):
+        guess[df_test.iloc[i, 0]] = knn.knn_prediction(df_test.iloc[i, 1:])
+
+    df_final = pd.DataFrame(list(guess.items()), columns=["Id", "Label"])
+    print(df_final)
+    df_final.to_csv("guess_final.csv", index=False)
