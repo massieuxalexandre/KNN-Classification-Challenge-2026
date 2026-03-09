09fbc451821f1ba205304923e3d711ab517da752
massieuxalexandre <massieuxalexandre@gmail.com>
2026-03-09 08:25:39 +0100

    a

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
         knn = Knn(k, data.get_data_train(), data.get_data_test(), data.get_data_labels())
 
         print("test n°", j+1, " : ", sep="")
-        # print("label reel - label trouve")
+        print("label reel - label trouve")
         for i in range(len(data.get_data_test())):
         # for i in range(5):
-            # print(data.get_data_test_labels().iloc[i], "-", knn.knn_prediction(data.get_data_test().iloc[i]))
+            print(data.get_data_test_labels().iloc[i], "-", knn.knn_prediction(data.get_data_test().iloc[i]))
             total += 1
             if (data.get_data_test_labels().iloc[i] == knn.knn_prediction(data.get_data_test().iloc[i])):
                 good += 1
